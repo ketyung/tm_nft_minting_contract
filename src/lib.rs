b1403d851200b81711b5e73aca9497a27dceb003
techchee <ketyung@techchee.com>
2022-11-06 16:36:13 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ use near_sdk::{
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    allowed_minting_caller : Option<AccountId>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -53,7 +54,8 @@ impl Contract {
      */
     #[init]
     pub fn init_with_metadata(owner_id: AccountId, name : String, symbol : String,
-    icon : Option<String>, base_uri : Option<String> ) -> Self {
+    icon : Option<String>, base_uri : Option<String>, 
+    allowed_minting_caller : Option<AccountId> ) -> Self {
         Self::new(
             owner_id,
             NFTContractMetadata {
@@ -65,6 +67,7 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
             },
+            allowed_minting_caller,
         )
     }
 
@@ -85,11 +88,13 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
             },
+            None, 
         )
     }
 
     #[init]
-    fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+    fn new(owner_id: AccountId, metadata: NFTContractMetadata,
+    allowed_minting_caller : Option<AccountId>) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
         Self {
@@ -101,6 +106,7 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            allowed_minting_caller : allowed_minting_caller,
         }
     }
 
@@ -113,16 +119,18 @@ impl Contract {
     /// `self.tokens.mint` will enforce `predecessor_account_id` to equal the `owner_id` given in
     /// initialization call to `new`.
     #[payable]
-    pub fn nft_mint(
-        &mut self,
-        token_id: TokenId,
-        receiver_id: AccountId,
-        token_metadata: TokenMetadata,
+    pub fn nft_mint(&mut self,token_id: TokenId,receiver_id: AccountId, token_metadata: TokenMetadata,
     ) -> Token {
-  //    deprecated  self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+
+        if self.allowed_minting_caller.is_none() ||
+        self.allowed_minting_caller.clone().unwrap() != env::predecessor_account_id(){
+            env::panic_str("Only cross-contract calling is allowed");
+        }
+
         self.tokens.internal_mint(token_id, receiver_id, Some(token_metadata))
-  
     }
+
+
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
